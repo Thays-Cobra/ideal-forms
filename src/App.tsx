d13931fdc982ec249838117f88bdaa20d7e19675
Thays Cobra Perez <thays.perez.13@gmail.com>
2025-10-22 18:12:48 -0300

    fix: comment unused routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { ThemeProvider } from "styled-components";
 import { BrowserRouter, Routes, Route } from "react-router";
 import { GlobalStyle } from "./config/theme/global";
 import { Login } from "./pages/login/login.page";
-
 import { WelcomePage } from "./pages/welcome/welcome.page";
 import { theme } from "./config/theme";
 
@@ -14,8 +13,8 @@ function App() {
 			<BrowserRouter>
 				<Routes>
 					<Route Component={Login} path="/" />
-					<Route Component={Login} path="/:id" />
-					<Route Component={Login} path="/:id/:access" />
+					{/* <Route Component={Login} path="/:id" />
+					<Route Component={Login} path="/:id/:access" /> */}
 					<Route Component={WelcomePage} path="/welcome" />
 				</Routes>
 			</BrowserRouter>
