bc8802126c3a3e81d914a01a002372e3bff26b53
Thays Cobra Perez <thays.perez.13@gmail.com>
2025-10-23 17:30:43 -0300

    chore: exclude welcome page

--- src/pages/welcome/welcome.page.tsx
+++
@@ -1,9 +0,0 @@
-import { Text } from "../../components/text";
-
-export function WelcomePage() {
-	return (
-		<div>
-			<Text className="WelcomePage">Bem vindo(a) à nossa página</Text>
-		</div>
-	);
-}
